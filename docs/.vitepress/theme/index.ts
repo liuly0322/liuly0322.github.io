6302a3cc98e02061ba75df1de4c7fdf266dab491
liuly0322 <liuly0322@mail.ustc.edu.cn>
2022-09-01 15:08:08 +0800

    pref: loading speed

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,26 +2,30 @@ import DefaultTheme from 'vitepress/theme'
 
 export default {
   ...DefaultTheme,
-  async setup() {
+  setup() {
     if (typeof window !== 'undefined') {
-      const SakanaWidget = (await import('./sakana')).default
+      ;(async () => {
+        const SakanaWidget = (await import('./sakana')).default
 
-      const sakana = document.createElement('div')
-      sakana.id = 'sakana'
-      sakana.style.position = 'fixed'
-      sakana.style.right = '0'
-      sakana.style.bottom = '0'
-      sakana.style.zIndex = '20'
-      document.body.appendChild(sakana)
+        const sakana = document.createElement('div')
+        sakana.id = 'sakana'
+        Object.assign(sakana.style, {
+          position: 'fixed',
+          right: '0',
+          bottom: '0',
+          zIndex: '20',
+        })
+        document.body.appendChild(sakana)
 
-      const takina = SakanaWidget.getCharacter('takina')
-      takina.initialState = {
-        ...takina.initialState,
-        i: 0.001,
-        d: 1,
-      }
-      SakanaWidget.registerCharacter('takina-slow', takina)
-      new SakanaWidget({ character: 'takina-slow' }).mount('#sakana')
+        const takina = SakanaWidget.getCharacter('takina')
+        takina.initialState = {
+          ...takina.initialState,
+          i: 0.001,
+          d: 1,
+        }
+        SakanaWidget.registerCharacter('takina-slow', takina)
+        new SakanaWidget({ character: 'takina-slow' }).mount('#sakana')
+      })()
     }
   },
 }
