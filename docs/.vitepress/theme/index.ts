cb9a0dd5a46ad05ca74e810a3a594147a03200d6
liuly <me@liuly.moe>
2023-04-07 14:37:22 +0800

    hide sakana widget for mobile users

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,24 +1,20 @@
 import DefaultTheme from 'vitepress/theme'
 import { onMounted } from 'vue'
 
+import './custom.css'
+
 const sakanaInit = async () => {
   const SakanaWidget = (await import('sakana-widget')).default
 
   const sakana = document.createElement('div')
   sakana.id = 'sakana'
-  Object.assign(sakana.style, {
-    position: 'fixed',
-    right: '0',
-    bottom: '0',
-    zIndex: '20',
-  })
   document.body.appendChild(sakana)
 
   const takina = SakanaWidget.getCharacter('takina')!
   takina.initialState = {
     ...takina.initialState,
-    i: 0.001,
-    d: 1,
+    i: 0.1,
+    d: 0.9,
   }
   SakanaWidget.registerCharacter('takina-slow', takina)
   new SakanaWidget({ character: 'takina-slow' }).mount('#sakana')
