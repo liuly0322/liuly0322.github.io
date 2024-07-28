1007acd4718b2130ec9907427d072a929b345263
liuly <me@liuly.moe>
2024-07-28 17:17:17 +0800

    Yuki.N

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,29 +1,19 @@
 import DefaultTheme from 'vitepress/theme'
 import { onMounted } from 'vue'
 
-import 'sakana-widget/lib/index.css';
 import './custom.css'
 
-const sakanaInit = async () => {
-  const SakanaWidget = (await import('sakana-widget')).default
-
-  const sakana = document.createElement('div')
-  sakana.id = 'sakana'
-  document.body.appendChild(sakana)
-
-  const takina = SakanaWidget.getCharacter('takina')!
-  takina.initialState = {
-    ...takina.initialState,
-    i: 0.1,
-    d: 0.9,
-  }
-  SakanaWidget.registerCharacter('takina-slow', takina)
-  new SakanaWidget({ character: 'takina-slow' }).mount('#sakana')
+const yukiInit = async () => {
+  const yuki = document.createElement('img')
+  yuki.id = 'yuki'
+  yuki.src = '/yuki.png'
+  yuki.alt = 'Nagato Yuki'
+  document.body.appendChild(yuki)
 }
 
 export default {
   ...DefaultTheme,
   setup() {
-    onMounted(sakanaInit)
+    onMounted(yukiInit)
   },
 }
