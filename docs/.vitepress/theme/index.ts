3d546b87203b93ccef2e5965fc3ed1b64d3367d1
liuly <me@liuly.moe>
2024-07-29 09:57:26 +0800

    Yuki.N

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,19 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
-import { onMounted } from 'vue'
-
 import './custom.css'
 
-const yukiInit = async () => {
-  const yuki = document.createElement('img')
-  yuki.id = 'yuki'
-  yuki.src = '/yuki.png'
-  yuki.alt = 'Nagato Yuki'
-  document.body.appendChild(yuki)
-}
-
-export default {
-  ...DefaultTheme,
-  setup() {
-    onMounted(yukiInit)
-  },
-}
+export default DefaultTheme
